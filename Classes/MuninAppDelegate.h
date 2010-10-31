35492668bf90966c8d91712ecf5618256ab17076
Leon Bogaert <leon@tim-online.nl>
2010-10-31 16:55:44 +0100

    Mockups toegevoegd, wat links, begin gemaakt met database (wordt nu automaticsh gekopieerd) en nog wat kleine dingetjes

--- Classes/MuninAppDelegate.h
+++ Classes/MuninAppDelegate.h
@@ -3,7 +3,7 @@
 //  Munin
 //
 //  Created by Leon Bogaert on 30-10-10.
-//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//  Copyright 2010 Tim_online. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
@@ -12,10 +12,20 @@
     
     UIWindow *window;
     UINavigationController *navigationController;
+	
+    NSManagedObjectModel *managedObjectModel;
+    NSManagedObjectContext *managedObjectContext;	    
+    NSPersistentStoreCoordinator *persistentStoreCoordinator;
 }
 
 @property (nonatomic, retain) IBOutlet UIWindow *window;
 @property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (NSString *)applicationDocumentsDirectory;
 
 @end
 
--- Classes/MuninAppDelegate.m
+++ Classes/MuninAppDelegate.m
@@ -23,6 +23,38 @@
     
     // Override point for customization after application launch.
     
+	NSManagedObjectContext *context = [self managedObjectContext];
+	NSError *error;
+	
+//	NSManagedObject *muninMaster = [NSEntityDescription
+//									   insertNewObjectForEntityForName:@"MuninMaster" 
+//									   inManagedObjectContext:context];
+//	[muninMaster setValue:@"http://munin.ping.uio.no" forKey:@"name"];
+//	
+//	NSManagedObject *host = [NSEntityDescription
+//										  insertNewObjectForEntityForName:@"Host" 
+//										  inManagedObjectContext:context];
+//	[host setValue:@"bimbo.ping.uio.no" forKey:@"name"];
+//	[host setValue:muninMaster forKey:@"muninMaster"];
+//	if (![context save:&error]) {
+//		NSLog(@"Whoops, couldn't save: %@", [error localizedDescription]);
+//	}
+	
+	//List all Masters:
+	NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
+	NSEntityDescription *entity = [NSEntityDescription 
+								   entityForName:@"MuninMaster" inManagedObjectContext:context];
+	[fetchRequest setEntity:entity];
+	NSArray *fetchedObjects = [context executeFetchRequest:fetchRequest error:&error];
+	for (NSManagedObject *info in fetchedObjects) {
+		NSLog(@"Name: %@", [info valueForKey:@"name"]);
+		for (NSManagedObject *host in [info valueForKey:@"hosts"]) {
+			//NSManagedObject *hosts = [info valueForKey:@"hosts"];
+			NSLog(@"Host: %@", host);
+		}
+	}        
+	[fetchRequest release];
+	
     // Add the navigation controller's view to the window and display.
     [window addSubview:navigationController.view];
     [window makeKeyAndVisible];
@@ -66,6 +98,111 @@
      Called when the application is about to terminate.
      See also applicationDidEnterBackground:.
      */
+    NSError *error;
+    if (managedObjectContext != nil) {
+        if ([managedObjectContext hasChanges] && ![managedObjectContext save:&error]) {
+			/*
+			 Replace this implementation with code to handle the error appropriately.
+			 
+			 abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development. If it is not possible to recover from the error, display an alert panel that instructs the user to quit the application by pressing the Home button.
+			 */
+			NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+			abort();
+        } 
+    }
+}
+
+#pragma mark -
+#pragma mark Core Data stack
+
+/**
+ Returns the managed object context for the application.
+ If the context doesn't already exist, it is created and bound to the persistent store coordinator for the application.
+ */
+- (NSManagedObjectContext *)managedObjectContext {
+	
+    if (managedObjectContext != nil) {
+        return managedObjectContext;
+    }
+	
+    NSPersistentStoreCoordinator *coordinator = [self persistentStoreCoordinator];
+    if (coordinator != nil) {
+        managedObjectContext = [NSManagedObjectContext new];
+        [managedObjectContext setPersistentStoreCoordinator: coordinator];
+    }
+    return managedObjectContext;
+}
+
+
+/**
+ Returns the managed object model for the application.
+ If the model doesn't already exist, it is created by merging all of the models found in the application bundle.
+ */
+- (NSManagedObjectModel *)managedObjectModel {
+	
+    if (managedObjectModel != nil) {
+        return managedObjectModel;
+    }
+    managedObjectModel = [[NSManagedObjectModel mergedModelFromBundles:nil] retain];    
+    return managedObjectModel;
+}
+
+
+/**
+ Returns the persistent store coordinator for the application.
+ If the coordinator doesn't already exist, it is created and the application's store added to it.
+ */
+- (NSPersistentStoreCoordinator *)persistentStoreCoordinator {
+	
+    if (persistentStoreCoordinator != nil) {
+        return persistentStoreCoordinator;
+    }
+	
+	NSString *storePath = [[self applicationDocumentsDirectory] stringByAppendingPathComponent:@"Munin.sqlite"];
+	/*
+	 Set up the store.
+	 For the sake of illustration, provide a pre-populated default store.
+	 */
+	NSFileManager *fileManager = [NSFileManager defaultManager];
+	// If the expected store doesn't exist, copy the default store.
+	if (![fileManager fileExistsAtPath:storePath]) {
+		NSString *defaultStorePath = [[NSBundle mainBundle] pathForResource:@"Munin" ofType:@"sqlite"];
+		if (defaultStorePath) {
+			[fileManager copyItemAtPath:defaultStorePath toPath:storePath error:NULL];
+		}
+	}
+	
+	NSURL *storeUrl = [NSURL fileURLWithPath:storePath];
+	
+	NSError *error;
+    persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel: [self managedObjectModel]];
+    if (![persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeUrl options:nil error:&error]) {
+		/*
+		 Replace this implementation with code to handle the error appropriately.
+		 
+		 abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development. If it is not possible to recover from the error, display an alert panel that instructs the user to quit the application by pressing the Home button.
+		 
+		 Typical reasons for an error here include:
+		 * The persistent store is not accessible
+		 * The schema for the persistent store is incompatible with current managed object model
+		 Check the error message to determine what the actual problem was.
+		 */
+		NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+		abort();
+    }    
+	
+    return persistentStoreCoordinator;
+}
+
+
+#pragma mark -
+#pragma mark Application's documents directory
+
+/**
+ Returns the path to the application's documents directory.
+ */
+- (NSString *)applicationDocumentsDirectory {
+	return [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
 }
 
 
@@ -80,6 +217,9 @@
 
 
 - (void)dealloc {
+    [managedObjectContext release];
+    [managedObjectModel release];
+    [persistentStoreCoordinator release];
 	[navigationController release];
 	[window release];
 	[super dealloc];
